971424e4c7f4752404b2e15a33a48ec87e760eb0
Krish Gupta <krishdevdb@gmail.com>
2021-08-23 08:44:29 +0530

    feat: commando setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
+const CommandoClient = require('discord.js-commando').CommandoClient
+const Structures = require('discord.js').Structures
+const path = require('path')
 
+const dotenv = require('dotenv')
+
+dotenv.config({
+  path: path.join(__dirname, '..', '.env')
+})
+
+Structures.extend('Guild', function (Guild) {
+  class MusicGuild extends Guild {
+    constructor (client, data) {
+      super(client, data)
+      this.musicData = {
+        queue: [],
+        isPlaying: false,
+        nowPlaying: null,
+        songDispatcher: null,
+        skipTimer: false,
+        loopSong: false,
+        loopQueue: false,
+        volume: 1
+      }
+    }
+  }
+  return MusicGuild
+})
+
+const client = new CommandoClient({
+  commandPrefix: process.env.BOT_PREFIX,
+  owner: process.env.BOT_OWNER
+})
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ['music', 'Music Commands'],
+    ['moderation', 'Server Moderation']
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands({
+    eval: true,
+    help: true,
+    prefix: true,
+    ping: true,
+    unknownCommand: false,
+    commandState: false
+  })
+  .registerCommandsIn(path.join(__dirname, 'commands'))
+
+client.once('ready', () => {
+  console.log('Ready!')
+
+  setInterval(
+    () =>
+      client.user.setActivity(
+        `${client.commandPrefix}help | Contributing to Open Source`,
+        {
+          type: 'LISTENING'
+        }
+      ),
+    10000
+  )
+})
+
+client.login(process.env.BOT_TOKEN)
